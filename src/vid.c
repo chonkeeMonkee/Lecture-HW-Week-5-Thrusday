5ed97d1ced26ffbbe77802e6f129930e8f1a7110
Alexander Montano-Leon <157240345+chonkeeMonkee@users.noreply.github.com>
2026-05-06 07:55:32 +0000

    Submission

--- src/vid.c
+++ src/vid.c
@@ -18,8 +18,10 @@ uint8_t cursor;
 int volatile* fb;
 int row, col, scroll_row;
 unsigned char* font;
-int WIDTH = 640;  // scan line width, default to 640
-int HEIGHT = 480;
+//int WIDTH = 640;  // scan line width, default to 640
+//int HEIGHT = 480;
+int WIDTH = 800;  // scan line width, default to 640
+int HEIGHT = 600;
 char* tab = "0123456789ABCDEF";
 
 int fbuf_init() {
@@ -27,20 +29,21 @@ int fbuf_init() {
   fb = (int*)0x200000;        // frame buffer at 2MB-4MB
   font = _binary_font_start;  // font bitmap
   //********* for 640x480 VGA mode *******************
+  /*
   *(volatile unsigned int *)(0x1000001c) = 0x2C77;
   *(volatile unsigned int *)(0x10120000) = 0x3F1F3F9C;
   *(volatile unsigned int *)(0x10120004) = 0x090B61DF;
   *(volatile unsigned int *)(0x10120008) = 0x067F1800;
   *(volatile unsigned int *)(0x10120010) = 0x200000; // at 2MB
-  *(volatile unsigned int *)(0x10120018) = 0x82B;
-  /********** for 800X600 SVGA mode ******************
+  *(volatile unsigned int *)(0x10120018) = 0x82B; */
+  /********** for 800X600 SVGA mode ******************/
   *(volatile unsigned int*)(0x1000001c) = 0x2CAC;  // 800x600
   *(volatile unsigned int*)(0x10120000) = 0x1313A4C4;
   *(volatile unsigned int*)(0x10120004) = 0x0505F6F7;
   *(volatile unsigned int*)(0x10120008) = 0x071F1800;
   *(volatile unsigned int*)(0x10120010) = 0x200000;
   *(volatile unsigned int*)(0x10120018) = 0x82B;
-  /**********/
+
   cursor = 219;  // cursor = row 127 in font bitmap
 }
 
@@ -173,17 +176,22 @@ int kprints(char* s) {
 }
 
 int krpx(int x) {
-  char c;
-  if (x) {
-    c = tab[x % 16];
-    krpx(x / 16);
+  char c[16];
+  int i = 0; 
+
+  while (x){
+    c[i] = tab[x % 16];
+    x = x/16;
+    ++i; 
   }
-  kputc(c);
+
+  while (i){
+    --i; 
+     kputc(c[i]);
+  }
 }
 
 int kprintx(int x) {
-  kputc('0');
-  kputc('x');
   if (x == 0)
     kputc('0');
   else
@@ -192,12 +200,21 @@ int kprintx(int x) {
 }
 
 int krpu(int x) {
-  char c;
-  if (x) {
-    c = tab[x % 10];
-    krpu(x / 10);
+  char c [10];
+  int i = 0; 
+  
+  while (x){
+    c[i] = tab[x % 10];
+    x = x/10; 
+    ++i; 
   }
-  kputc(c);
+
+  while (i){
+    --i; 
+    kputc(c[i]);
+  }
+
+  return 0; 
 }
 
 int kprintu(int x) {
